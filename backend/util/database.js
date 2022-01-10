2b114ebecd8ec5c32c38cece915b931dad66079b
baris <barisbll.dev@gmail.com>
2022-01-10 18:15:11 +0100

    secret file is being put to the .gitignore

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
 
+const secret = require("../secret");
+
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "languagedbfromscript",
-  password: "mysql",
+  password: `${secret.mysqlSecret}`,
 });
 
 module.exports = pool.promise();
